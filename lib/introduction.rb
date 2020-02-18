d04a08f2ffca94f9380d3122dd691e19082fd814
Ethan Davidson <theejdavidson@gmail.com>
2020-02-18 17:45:35 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
 def introduction(name)
   puts "Hello, my name is #{name}"
 end
+
+def introduction_with_language(name, language)
+  puts "Hello, my name is #{name} and I am learning to program in #{language}"
+end
